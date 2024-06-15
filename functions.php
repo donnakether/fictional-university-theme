81e1785b0e1534f6a8a12449977a8c92fd490f99
DevDon <donnakether@gmail.com>
2024-06-15 13:58:38 -0300

    Creating menu support and calling on theme  files

--- functions.php
+++ functions.php
@@ -10,9 +10,13 @@ function university_files() {
 
 add_action('wp_enqueue_scripts','university_files');
 
-function unviversity_features(){
-    add_theme_support('title-tag');
+function university_features(){
 
+    register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    register_nav_menu('footerLocationOne', 'Footer Location One');
+    register_nav_menu('footerLocationTwo', 'Footer Location Two');
+    
+    add_theme_support('title-tag');
 }
 
-add_action('after_theme_setup','university_features');
+add_action('after_setup_theme','university_features');
