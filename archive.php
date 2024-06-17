c57d493287ee641875b1f9a4c5f8028c0919acd9
DevDon <donnakether@gmail.com>
2024-06-17 18:27:49 -0300

    Fix archive page loop

--- archive.php
+++ archive.php
@@ -1,37 +1,38 @@
-<?php 
-    get_header();
+<?php get_header();?>
 
-    while(have_posts()) {
-        the_post(); ?> 
-        <div class="page-banner">
-           <div class="page-banner__bg-image" style="background-image: url(<?php echo 
-            get_theme_file_uri('images/ocean.jpg');?>)"></div>
-           <div class="page-banner__content container container--narrow">
-             <h1 class="page-banner__title"><?php the_archive_title()?></h1>
-             <div class="page-banner__intro">
-               <p><?php the_archive_description()?></p>
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg');?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_archive_title();?></h1>
+        <div class="page-banner__intro">
+          <p><?php the_archive_description();?></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+      <?php
+        while(have_posts()) {
+          the_post(); ?>
+           <div class="post-item">
+            <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+             <div class="metabox">
+              <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y');?> in <?php echo get_the_category_list(', ');?> </p>
              </div>
-            </div>
-        </div>
 
-       <div class="container container--narrow page-section">
+             <div class="generic-content">
+              <?php the_excerpt();?>
+              <p><a class="btn btn--blue" href="<?php the_permalink();?>">Continue reading &raquo;</a></p>
+             </div>
 
-       <div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-          <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>">
-            <i class="fa fa-home" aria-hidden="true"></i> Blog Home </a> 
-          <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y');?> in <?php echo get_the_category_list(', ');?></span>
-        </p>
-       </div>
+          
 
-        <div class="generic-content">
-        <?php the_content();?>
-        </div>
-       </div>
-
-
-
-
+           </div>
         <?php }
-    get_footer();
-?>
+        echo paginate_links();
+      ?>
+    </div>     
+
+<?php get_footer();?>
+
