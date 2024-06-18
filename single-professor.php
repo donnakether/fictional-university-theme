d784124777976efd209d1ce110196a9a479c42b9
DevDon <donnakether@gmail.com>
2024-06-18 17:39:03 -0300

    Post types relations

--- single-professor.php
+++ single-professor.php
@@ -16,14 +16,6 @@
 
        <div class="container container--narrow page-section">
 
-       <div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-          <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>">
-            <i class="fa fa-home" aria-hidden="true"></i> Events Home </a> 
-          <span class="metabox__main"><?php the_title(); ?></span>
-        </p>
-       </div>
-
         <div class="generic-content">
         <?php the_content(); ?>
         </div>
@@ -34,7 +26,7 @@
         if ($relatedPrograms) {
 
         echo '<hr class="section-break">';
-        echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+        echo '<h2 class="headline headline--medium">Subject(s) Tauhght</h2>';
         echo '<ul class="link-list min-list">';
 
         foreach($relatedPrograms as $program) {?>
